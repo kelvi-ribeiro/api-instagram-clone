84f6e022168320777ae8bd9da34d0e241b0bbe81
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-01-02 09:35:15 -0200

    Aula 129. Iniciando o projeto da API atraves do package.json e criando config inicial do server

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+    bodypParser = require('body-parser'),
+    mongodb = require('mongodb');
+
+var app = express();
+
+app.use(bodypParser.urlencoded({extended:true}));
+app.use(bodypParser.json);
+
+var port = 8080;
+
+app.listen(port);
+
+console.log('Servidor HTTP está escutando na porta ' + port);
+
