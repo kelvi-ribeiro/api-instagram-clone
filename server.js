f43d7d821cd1a786a13b6eeabd7c7f06910dcb0b
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-01-02 10:11:30 -0200

    Aula 131. Method Post: Incluindo documentos no MongoDB

--- server.js
+++ server.js
@@ -1,19 +1,66 @@
 var express = require('express'),
-    bodypParser = require('body-parser'),
-    mongodb = require('mongodb');
-
+    bodyParser = require('body-parser'),
+    mongodb = require('mongodb').MongoClient,
+    objectID = require('mongodb').ObjectId;
+ 
 var app = express();
-
-app.use(bodypParser.urlencoded({extended:true}));
-app.use(bodypParser.json());
-
+ 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+ 
 var port = 8080;
-
+ 
 app.listen(port);
-
-console.log('Servidor HTTP está escutando na porta ' + port);
-
-app.get('/',function(req,res){
-    res.json({msg:'olá'});
+ 
+var dbName = 'instagram';
+var mongoURL = 'mongodb://localhost:27017/' + dbName;
+ 
+var connMongoDB = function(data) {
+  mongodb.connect(mongoURL, { useNewUrlParser: true }, function(err, client) {
+    var db = client.db(dbName);
+    query(db, data);
+    client.close();
+  });
+}
+ 
+function query(db, data) {
+  var collection = db.collection(data.collection);
+  switch (data.operacao) {
+    case 'atualizar':
+      collection.update(data.where, data.set);
+      break;
+    case 'inserir':
+      collection.insertOne(data.dados, data.callback);
+      break;
+    case 'pesquisar':
+      collection.find(data.dados).toArray(data.callback);
+      break;
+    case 'remover':
+      data.where._id = objectID(data.where._id);
+      collection.remove(data.where, data.callback);
+      break;
+  }
+}
+ 
+console.log('Servidor HTTP escutando na porta ' + port);
+ 
+app.get('/', function(req, res){
+  res.send({msg: 'Olá'});
 });
-
+ 
+app.post('/api', function(req, res){
+  var data = req.body;
+  var dados = {
+    operacao: 'inserir',
+    dados: data,
+    collection: 'postagens',
+    callback: function(err, records){
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(records);
+      }
+    }
+  }
+  connMongoDB(dados);
+});
