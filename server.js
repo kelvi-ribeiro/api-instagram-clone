fcb14629e85e857ffc9a838d1da96324489e5694
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2019-01-02 09:46:07 -0200

    Aula 130. Dando olá mundo para testar a api

--- server.js
+++ server.js
@@ -5,11 +5,15 @@ var express = require('express'),
 var app = express();
 
 app.use(bodypParser.urlencoded({extended:true}));
-app.use(bodypParser.json);
+app.use(bodypParser.json());
 
 var port = 8080;
 
 app.listen(port);
 
 console.log('Servidor HTTP está escutando na porta ' + port);
+
+app.get('/',function(req,res){
+    res.json({msg:'olá'});
+});
 
